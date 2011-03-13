349b9a4a7b6fe809e634cead669372bd34f19979
Jannis Leidel <jannis@leidel.info>
2011-03-13 09:05:21 +0800

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name="labcoat",
+    author="Marty Alchin",
+    author_email="marty@martyalchin.com",
+    version="0.1a1",
+    packages=["labcoat"],
+    license="BSD",
+    url="https://github.com/gulopine/labcoat",
+    classifiers=[
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: BSD License",
+      "Topic :: Software Development :: Testing",
+    ],
+)
